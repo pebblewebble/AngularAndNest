4bb9778567e90143b8d9db952178dda6710fb65c
pebblewebble <142425284+pebblewebble@users.noreply.github.com>
2024-12-27 04:17:37 +0800

    idk

--- tasks-ui/src/app/app.component.ts
+++ tasks-ui/src/app/app.component.ts
@@ -1,12 +1,41 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+import { TaskService } from './tasks.service';
+interface Task {
+  id: number;
+  name: string;
+  completed: boolean;
+}
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'tasks-ui';
+  tasks: Task[];
+  task: string;
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  constructor(
+    private taskService: TaskService,
+  ) {
+    this.tasks = [];
+    this.task = '';
+  }
+  title = 'task-ui';
+  ngOnInit() {
+    this.taskService.getTasks().subscribe((data) => {
+      console.log(data);
+      this.tasks = data as Task[];
+    });
+  }
+
+  addTask(task: string) {
+    this.taskService.addTask(task).subscribe();
+    this.task ='';
+  }
+
+  deleteTask(id: number) {
+    this.taskService.deleteTask(id).subscribe((data) => {
+      console.log(data);
+    });
+  }
 }
